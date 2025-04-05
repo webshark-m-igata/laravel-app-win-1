e477e148b247f67a9b7051b3a36ce890b267bc63
motoaki <m.igata@webshark.co.jp>
2025-04-06 03:55:46 +0900

    refactor(AppServiceProvider): 本番環境でHTTPSを強制するように変更
    本番環境において、URLのスキームをHTTPSに強制するように変更しました。これにより、セキュリティが向上し、安全な通信が保証されます。また、不要になったVite::prefetchの呼び出しを削除しました。

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,8 +19,14 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        Vite::prefetch(concurrency: 3);
+    //public function boot(): void
+    //{
+    //    Vite::prefetch(concurrency: 3);
+    //}
+    public function boot()
+{
+    if ($this->app->environment('production')) {
+        URL::forceScheme('https');
     }
+}
 }
